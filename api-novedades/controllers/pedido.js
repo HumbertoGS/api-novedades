4856550f47f3eeb548c47e0c41e46bcf524f1278
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-15 01:13:06 -0500

    se agrega funcion getpedido y cambio de estado para detalles y pedido

--- api-novedades/controllers/pedido.js
+++ api-novedades/controllers/pedido.js
@@ -1,5 +1,5 @@
 //Dependencias
-
+import { Sequelize, Op } from "sequelize";
 //Modulos
 // import store from "../Connection/dbposgres.js";
 
@@ -12,6 +12,13 @@ export default function (sentences) {
     const date = new Date();
     const options = { month: "long", day: "numeric", year: "numeric" };
     return date.toLocaleDateString("en-US", options);
+  }
+
+  function addZeros(str) {
+    const desiredLength = 5;
+    const currentLength = str.length;
+    const zerosToAdd = desiredLength - currentLength;
+    return "0".repeat(zerosToAdd) + str;
   }
 
   async function insert({ orden, totales }) {
@@ -41,7 +48,7 @@ export default function (sentences) {
 
     return {
       detalle: {
-        num_pedido: `0000${num_venta}`,
+        num_pedido: addZeros(num_venta.toString()),
         fecha: fecha(),
         total: totales.total,
       },
@@ -55,12 +62,115 @@ export default function (sentences) {
   }
 
   async function getPedido() {
-    return;
+    const pedido = await sentences.selectJoin(
+      "db-novedades",
+      "resumen_orden",
+      ["*"],
+      {
+        id_estado: 1,
+      },
+      [
+        {
+          name: "orden",
+          as: "ordens",
+          required: true,
+          sourceKey: "num_venta",
+          select: ["id_cliente", "num_venta"],
+          where: {},
+          include: [
+            {
+              name: "cliente",
+              as: "id_cliente_cliente",
+              required: true,
+              select: ["id", "cedula", "nombre", "apellido"],
+              where: {},
+            },
+          ],
+        },
+      ],
+      true,
+      [["id", "DESC"]]
+    );
+
+    let repetido = 0;
+
+    let datos = [];
+    pedido.map((item) => {
+      if (repetido !== item.num_venta) {
+        datos.push({
+          id: item.id,
+          num_pedido: item.num_venta,
+          num_producto: item.total_pedido,
+          total: "$" + item.total,
+          transferencia: item.transferencia,
+          status: item.id_estado,
+          estado: item.estado,
+          id_cliente: item["ordens.id_cliente"],
+          num_identificacion: item["ordens.id_cliente_cliente.cedula"],
+          nombre_completo: `${item["ordens.id_cliente_cliente.apellido"]} ${item["ordens.id_cliente_cliente.nombre"]}`,
+        });
+      }
+      repetido = item.num_venta;
+    });
+
+    return datos;
   }
 
-  async function getPedidoDetalle(data) {
-    return;
+  async function getPedidoDetalle({ num_venta }) {
+    const detalle = await sentences.selectJoin(
+      "db-novedades",
+      "orden",
+      ["id", "num_venta", "cantidad", "total_producto", "estado"],
+      {
+        num_venta,
+      },
+      [
+        {
+          name: "producto",
+          as: "id_producto_producto",
+          required: true,
+          select: ["nombre", "precio", "categoria", "stock", "codigo"],
+          where: {},
+          include: [
+            {
+              name: "categoria",
+              as: "categoria_categorium",
+              required: true,
+              select: ["nombre"],
+              where: {},
+            },
+          ],
+        },
+      ],
+      true,
+      [["id", "ASC"]]
+    );
+
+    let datos = detalle.map((item) => {
+      return {
+        id: item.id,
+        num_venta: item.num_venta,
+        codigo: item["id_producto_producto.codigo"],
+        producto: item["id_producto_producto.nombre"],
+        precio_unidad: "$" + item["id_producto_producto.precio"],
+        cantidad: item.cantidad,
+        total: "$" + item.total_producto,
+        stock: item["id_producto_producto.stock"],
+        estado: item.estado,
+        nombre_categoria:
+          item["id_producto_producto.categoria_categorium.nombre"],
+        id_categoria: item["id_producto_producto.categoria_categorium.id"],
+      };
+    });
+
+    return datos;
   }
 
-  return { insert, getPedido, getPedidoDetalle };
+  async function cambiarEstadoPedidoDetalle({ id, estado }) {
+    console.log(estado);
+    console.log(id);
+    return await sentences.update("db-novedades", "orden", { estado }, { id });
+  }
+
+  return { insert, getPedido, getPedidoDetalle, cambiarEstadoPedidoDetalle };
 }
